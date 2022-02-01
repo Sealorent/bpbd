b524f7983205771faf651f50e9cdf76ec7d95453
SeaLorent <91618729+Sealorent@users.noreply.github.com>
2022-02-01 08:02:58 +0700

    api

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:bpbd/ui/pages/pages.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MainPage(),
+      home: SplashScreen(),
     );
   }
 }
